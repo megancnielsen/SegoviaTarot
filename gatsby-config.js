547a646ee33e751d7a6f1f3983e2a17a7e23a175
IFletcher <Isiahfletcher7@gmail.com>
2020-08-12 21:44:56 -0600

    updating menu

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,51 +1,51 @@
-const config = require('./config.js');
+const config = require('./siteData.ts');
 
 module.exports = {
-  pathPrefix: config.pathPrefix,
-  siteMetadata: {
-    title: config.siteTitle,
-  },
-  plugins: [
-    'gatsby-plugin-sass',
-    'gatsby-plugin-offline',
-    // `gatsby-transformer-sharp`, // TODO: look into if we need this later
-    // `gatsby-plugin-sharp`,
-    // `gatsby-plugin-sitemap`,
-    `gatsby-plugin-react-helmet`,
-    `gatsby-plugin-typescript`,
-    {
-      resolve: `gatsby-plugin-manifest`,
-      options: {
-        name: config.manifestName,
-        short_name: config.manifestShortName,
-        start_url: config.pathPrefix || config.manifestStartUrl,
-        background_color: config.manifestBackgroundColor,
-        theme_color: config.manifestThemeColor,
-        display: config.manifestDisplay,
-        icon: config.manifestIcon, // This path is relative to the root of the site.
-      },
+    pathPrefix: config.pathPrefix,
+    siteMetadata: {
+        title: config.siteTitle,
     },
-    {
-      resolve: `gatsby-source-filesystem`,
-      options: {
-        name: `assets`,
-        path: `${__dirname}/src/assets/`,
-      },
-    },
-    {
-      resolve: `gatsby-plugin-prefetch-google-fonts`,
-      options: {
-        fonts: [
-          {
-            family: `Cormorant Garamond`,
-            variants: [`400`, `500`, `700`],
-          },
-          {
-            family: `Montserrat`,
-            variants: [`400`, `500`, `700`],
-          },
-        ],
-      },
-    },
-  ],
+    plugins: [
+        'gatsby-plugin-sass',
+        'gatsby-plugin-offline',
+        // `gatsby-transformer-sharp`, // TODO: look into if we need this later
+        // `gatsby-plugin-sharp`,
+        // `gatsby-plugin-sitemap`,
+        `gatsby-plugin-react-helmet`,
+        `gatsby-plugin-typescript`,
+        {
+            resolve: `gatsby-plugin-manifest`,
+            options: {
+                name: config.manifestName,
+                short_name: config.manifestShortName,
+                start_url: config.pathPrefix || config.manifestStartUrl,
+                background_color: config.manifestBackgroundColor,
+                theme_color: config.manifestThemeColor,
+                display: config.manifestDisplay,
+                icon: config.manifestIcon, // This path is relative to the root of the site.
+            },
+        },
+        {
+            resolve: `gatsby-source-filesystem`,
+            options: {
+                name: `assets`,
+                path: `${__dirname}/src/assets/`,
+            },
+        },
+        {
+            resolve: `gatsby-plugin-prefetch-google-fonts`,
+            options: {
+                fonts: [
+                    {
+                        family: `Cormorant Garamond`,
+                        variants: [`400`, `500`, `700`],
+                    },
+                    {
+                        family: `Montserrat`,
+                        variants: [`400`, `500`, `700`],
+                    },
+                ],
+            },
+        },
+    ],
 };
---
+++ src/components/header.tsx
@@ -0,0 +1,36 @@
+import React, { useState } from 'react';
+import Nav from './nav';
+import { Link } from 'gatsby';
+
+interface ISidebar extends IProps {
+    fullMenu: boolean;
+}
+
+const Sidebar: React.FC<ISidebar> = ({ fullMenu }) => {
+    const [headerOpen, toggleHeader] = useState(false);
+    return (
+        <header id="header" className={`${fullMenu ? '' : 'alt'}`}>
+            <h1>
+                <Link to="/">Solid State</Link>
+            </h1>
+
+            <nav>
+                <div className={`${headerOpen ? 'is-menu-visible' : ''}`}>
+                    <Nav onMenuToggle={() => toggleHeader(!headerOpen)} />
+                </div>
+                <a
+                    href="#menu"
+                    onClick={e => {
+                        e.preventDefault();
+                        toggleHeader(!headerOpen);
+                    }}
+                    className="menuToggle"
+                >
+                    <span>Menu</span>
+                </a>
+            </nav>
+        </header>
+    );
+};
+
+export default Sidebar;

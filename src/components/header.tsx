e9409c1ca20c2a5cc35fb868b973455056293901
IFletcher <Isiahfletcher7@gmail.com>
2020-08-12 21:52:06 -0600

    renaming sidebar to header

--- src/components/header.tsx
+++ src/components/header.tsx
@@ -9,27 +9,29 @@ interface ISidebar extends IProps {
 const Sidebar: React.FC<ISidebar> = ({ fullMenu }) => {
     const [headerOpen, toggleHeader] = useState(false);
     return (
-        <header id="header" className={`${fullMenu ? '' : 'alt'}`}>
-            <h1>
-                <Link to="/">Solid State</Link>
-            </h1>
+        <>
+            <header id="header" className={`${fullMenu ? '' : 'alt'}`}>
+                <h1>
+                    <Link to="/">Solid State</Link>
+                </h1>
 
-            <nav>
-                <div className={`${headerOpen ? 'is-menu-visible' : ''}`}>
-                    <Nav onMenuToggle={() => toggleHeader(!headerOpen)} />
-                </div>
-                <a
-                    href="#menu"
-                    onClick={e => {
-                        e.preventDefault();
-                        toggleHeader(!headerOpen);
-                    }}
-                    className="menuToggle"
-                >
-                    <span>Menuss</span>
-                </a>
-            </nav>
-        </header>
+                <nav>
+                    <a
+                        href="#menu"
+                        onClick={e => {
+                            e.preventDefault();
+                            toggleHeader(!headerOpen);
+                        }}
+                        className="menuToggle"
+                    >
+                        <span>Menu</span>
+                    </a>
+                </nav>
+            </header>
+            <div className={`${headerOpen ? 'is-menu-visible' : ' '}`}>
+                <Nav onMenuToggle={() => toggleHeader(!headerOpen)} />
+            </div>
+        </>
     );
 };
 

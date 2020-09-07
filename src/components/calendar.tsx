3fe184956d5dc0b7db013873f8f5375638ce1f96
IFletcher <Isiahfletcher7@gmail.com>
2020-09-07 10:06:59 -0600

    adding calendar component

---
+++ src/components/calendar.tsx
@@ -0,0 +1,5 @@
+import React from 'react';
+
+export default () => {
+    return <h1>Calendar App!</h1>;
+};
---
+++ src/pages/schedule-an-appointment.tsx
@@ -0,0 +1,9 @@
+import React from 'react';
+import Layout from '../components/layout';
+import Calendar from '../components/calendar';
+
+export default () => {
+    <Layout fullMenu>
+        <Calendar />
+    </Layout>;
+};
